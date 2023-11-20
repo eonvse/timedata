d866f59911d0d1b7cfb09712e6d080df1352cbd1
Сергей <eonvse@ya.ru>
2023-11-20 15:05:47 +0800

    Edit tooltip.absolute-bottom

--- resources/views/components/tooltip/absolute-bottom.blade.php
+++ resources/views/components/tooltip/absolute-bottom.blade.php
@@ -4,7 +4,7 @@
     x-on:mouseleave="tooltip = false">
     {{ $slot }}
     @isset($tooltip)
-    <div x-cloak x-show="tooltip" {{ $attributes->merge(['class'=>'absolute z-50 left-0 bottom-0 p-2 overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto tabular-nums']) }}>
+    <div x-cloak x-show="tooltip" {{ $attributes->merge(['class'=>'absolute z-50 left-0 bottom-0 p-2 overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto tabular-nums font-normal']) }}>
         {{ $tooltip }}
     </div>
     @endisset
