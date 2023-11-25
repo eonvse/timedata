27190277cef6634a4dbfeba1760a289364086766
Сергей <eonvse@ya.ru>
2023-11-25 14:03:10 +0800

    week-table: tooltip справа. team-event: поправлено расположение элементов.

--- resources/views/components/item/team-event.blade.php
+++ resources/views/components/item/team-event.blade.php
@@ -3,11 +3,11 @@
     $weekDays = array( 1 => 'Пн' , 'Вт' , 'Ср' , 'Чт' , 'Пт' , 'Сб' , 'Вс' );
 @endphp
 
-<div class="flex text-sm hover:bg-gray-600 hover:text-white border-b space-y-1">
+<div class="flex text-sm hover:bg-gray-600 hover:text-white border-b space-y-1 items-center">
 		<a href="{{ route('info.time-event',['id'=>$item->id, 'edit'=>0])}}" 
 			class="w-full flex space-x-2 border-r px-1 items-center grow after:content-['_↗']">
-			<span class="tabular-nums">{{ $item->dayFormat }}</span>
-			<span class="tabular-nums">{{ $item->startFormat }} - {{ $item->endFormat }}</span>
+			<span class="tabular-nums flex-1">{{ $item->dayFormat }}</span>
+			<span class="tabular-nums flex-none">{{ $item->startFormat }} - {{ $item->endFormat }}</span>
 			<span class="grow">{{ $item->title ?? '' }}</span>
 		</a>
 		<div class="">
--- resources/views/components/weektab-wire/event.blade.php
+++ resources/views/components/weektab-wire/event.blade.php
@@ -10,17 +10,21 @@
     <span class="ml-2 font-medium overflow-hidden">{{ $time_event->name }}.</span>
     <span class="ml-2 overflow-hidden">"{{ $time_event->title }}"</span>
 
-    <div x-cloak x-show="tooltip" class="grid grid-cols-3 absolute z-50 left-0 bottom-0 p-1 {{ $time_event->color }} text-black overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto tabular-nums">
-        <div class="whitespace-pre-line">Всего участников: {{ $time_event->all_u }}
-            Было: {{ $time_event->b_u }}
-            Не было: {{ $time_event->n_u }}
+    <div x-cloak x-show="tooltip" class="absolute z-50 right-0 top-0 bottom-0 p-1 {{ $time_event->color }} text-black overflow-hidden shadow-xl transform transition-all sm:w-1/2 sm:mx-auto tabular-nums">
+        <div class="text-lg bg-white p-2"><span class="font-bold">{{ $time_event->name }}:</span> {{ $time_event->title }}</div>
+        <div class="grid grid-cols-2 space-x-2 p-3">
+            <div class="whitespace-pre-line">Всего участников: {{ $time_event->all_u }}
+                Было: {{ $time_event->b_u }}
+                Не было: {{ $time_event->n_u }}
+            </div>
+            <div class="whitespace-pre-line">Заметок: {{ $time_event->notes }}
+                Файлов: {{ $time_event->files }}
+            </div>            
         </div>
-        <div class="whitespace-pre-line">Заметок: {{ $time_event->notes }}
-            Файлов: {{ $time_event->files }}
-        </div>
+
         <div>
-            <div>Последняя заметка</div>
             @if (!empty($time_event->last_note))
+            <div>Последняя заметка</div>
             <span class="italic text-black">
                 <svg class="h-4 w-4 inline-flex"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4" />  <line x1="12" y1="11" x2="12" y2="11.01" />  <line x1="8" y1="11" x2="8" y2="11.01" />  <line x1="16" y1="11" x2="16" y2="11.01" /></svg>
                 {{ $time_event->last_note }}
