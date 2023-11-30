d1351153291bab6928fe5a048e648c04aec17f9d
Сергей <eonvse@ya.ru>
2023-11-30 20:36:01 +0800

    Добавлены спиннеры на переключение месяца.

--- resources/views/components/monthtab-wire/switch-month.blade.php
+++ resources/views/components/monthtab-wire/switch-month.blade.php
@@ -1,4 +1,7 @@
 <div class="flex ml-6">
+    <x-spinner wire:loading wire:target="prevMonth" />
+    <x-spinner wire:loading wire:target="currentMonth" />
+    <x-spinner wire:loading wire:target="nextMonth" />
     <button wire:click="prevMonth" title="Предыдущий месяц" class="hover:shadow">
         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
